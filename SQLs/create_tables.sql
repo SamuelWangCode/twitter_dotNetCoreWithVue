54e40d83f275d0554f67e24cc5c2554447a08ceb
Sky-1751862 <564318045@qq.com>
2019-07-06 10:50:42 +0800

    Update create_tables.sql

--- SQLs/create_tables.sql
+++ SQLs/create_tables.sql
@@ -25,23 +25,19 @@ DROP SEQUENCE  SEQ_USER_PUBLIC_INFO;
 
 
 ----------User_Public_Info----------------
-CREATE SEQUENCE seq_user_public_info
-MINVALUE 1
-NOMAXVALUE
-NOCYCLE
-START WITH 1
-INCREMENT BY 1
-CACHE 10;
 
 
 CREATE TABLE User_Public_Info (
-  user_id INTEGER PRIMARY KEY,
+  user_id INTEGER GENERATED ALWAYS AS IDENTITY,
   user_nickname VARCHAR2(20) NOT NULL,
   user_register_time VARCHAR2(30) NOT NULL,
   user_self_introduction VARCHAR2(255) NOT NULL,
   user_followers_num INTEGER NOT NULL,
-  user_follows_num INTEGER NOT NULL
+  user_follows_num INTEGER NOT NULL,
+  CONSTRAINT pk_user_public_info 
+	PRIMARY KEY(user_id)
 );
+
 
 
 
@@ -62,16 +58,9 @@ CREATE TABLE User_Private_Info(
 
 ------------------------------------------
 ------------Avatar_Image-------------
-CREATE SEQUENCE seq_avatar_image
-MINVALUE 1
-NOMAXVALUE
-NOCYCLE
-START WITH 1
-INCREMENT BY 1
-CACHE 10;
 
 CREATE TABLE Avatar_Image ги
-  avatar_image_id INTEGER,
+  avatar_image_id INTEGER GENERATED ALWAYS AS IDENTITY,
   user_id INTEGER,
   avatar_image_in_use INTEGER NOT NULL,
   CONSTRAINT pk_avatar_image
@@ -86,21 +75,16 @@ CREATE TABLE Avatar_Image ги
 ------------------------------------------
 
 -----------------Private_Letter-----------
-CREATE SEQUENCE seq_private_letter
-MINVALUE 1
-NOMAXVALUE
-NOCYCLE
-START WITH 1
-INCREMENT BY 1
-CACHE 10;
 
 CREATE TABLE Private_Letter(
-  private_letter_id INTEGER PRIMARY KEY,
+  private_letter_id INTEGER GENERATED ALWAYS AS IDENTITY,
   private_letter_content VARCHAR2(255) NOT NULL,
   private_letter_is_read INTEGER NOT NULL,
-  private_letter_create_time DATE NOT NULL,
+  private_letter_create_time VARCHAR2(30) NOT NULL,
   private_letter_sender_id INTEGER NOT NULL,
   private_letter_receiver_id INTEGER NOT NULL,
+  CONSTRAINT pk_private_letter 
+	PRIMARY KEY (private_letter_id),
   CONSTRAINT fk_private_letter_1
     FOREIGN KEY (private_letter_sender_id)
       REFERENCES User_Public_Info(user_id)
@@ -133,16 +117,8 @@ CREATE TABLE Relation(
 );
 ---------------------------------------------
 -------------Message-------------------------
-CREATE SEQUENCE seq_message
-MINVALUE 1
-NOMAXVALUE
-NOCYCLE
-START WITH 1
-INCREMENT BY 1
-CACHE 10;
-
 CREATE TABLE Message(
-  message_id INTEGER PRIMARY KEY,
+  message_id INTEGER GENERATED ALWAYS AS IDENTITY,
   message_content VARCHAR2(280) NOT NULL,
   message_create_time VARCHAR2(30) NOT NULL,
   message_agree_num INTEGER NOT NULL,
@@ -152,6 +128,8 @@ CREATE TABLE Message(
   message_has_image INTEGER NOT NULL,
   message_sender_user_id INTEGER NOT NULL,
   message_heat INTEGER NOT NULL,
+  CONSTRAINT pk_message 
+	PRIMARY KEY (message_id),
   CONSTRAINT fk_message
     FOREIGN KEY (message_sender_user_id)
       REFERENCES User_Public_Info(user_id)
@@ -180,36 +158,24 @@ CREATE TABLE Transpond(
 );
 ----------------------------------------------
 ------------------Message_Image---------------
-CREATE SEQUENCE seq_message_image
-MINVALUE 1
-NOMAXVALUE
-NOCYCLE
-START WITH 1
-INCREMENT BY 1
-CACHE 10;
 
 CREATE TABLE Message_Image(
-  message_id INTEGER PRIMARY KEY,
+  message_id INTEGER GENERATED ALWAYS AS IDENTITY,
   message_image_count INTEGER NOT NULL,
+  CONSTRAINT pk_message_image
+	PRIMARY KEY (message_id),
   CONSTRAINT fk_message_image FOREIGN KEY
     (message_id) REFERENCES Message(message_id)
       ON DELETE CASCADE
 );
 -------------------------------------------------
 ------------------At_User------------------------
-CREATE SEQUENCE seq_at_user
-MINVALUE 1
-NOMAXVALUE
-NOCYCLE
-START WITH 1
-INCREMENT BY 1
-CACHE 10;
 
 CREATE TABLE At_User(
   at_user_id INTEGER PRIMARY KEY,
   message_id INTEGER NOT NULL,
   user_id INTEGER NOT NULL,
-  at_time DATE NOT NULL,
+  at_time VARCHAR2(30) NOT NULL,
   at_is_read INTEGER NOT NULL,
   CONSTRAINT fk_at_user_1 FOREIGN KEY
     (message_id) REFERENCES
@@ -224,19 +190,14 @@ CREATE TABLE At_User(
 CREATE INDEX a_u_user_id_index ON At_User(user_id, at_time);
 --------------------------------------------------
 --------------Like--------------------------------
-CREATE SEQUENCE seq_likes
-MINVALUE 1
-NOMAXVALUE
-NOCYCLE
-START WITH 1
-INCREMENT BY 1
-CACHE 10;
 
 CREATE TABLE Likes(
-  likes_id INTEGER PRIMARY KEY,
+  likes_id INTEGER GENERATED ALWAYS AS IDENTITY,
   likes_user_id INTEGER NOT NULL,
   likes_message_id INTEGER NOT NULL,
-  likes_time DATE NOT NULL,
+  likes_time VARCHAR2(30) NOT NULL,
+  CONSTRAINT pk_likes
+	PRIMARY KEY (likes_id),
   CONSTRAINT fk_likes_1 FOREIGN KEY
     (likes_user_id) REFERENCES
       User_Public_Info(user_id)
@@ -251,21 +212,17 @@ CREATE INDEX l_user_id_index ON Likes(likes_user_id, likes_time);
 CREATE INDEX l_message_id_index ON Likes(likes_message_id, likes_time);
 
 -----------------Comment--------------------------
-CREATE SEQUENCE seq_comment_on_Message
-MINVALUE 1
-NOMAXVALUE
-NOCYCLE
-START WITH 1
-INCREMENT BY 1
-CACHE 10;
+
 
 CREATE TABLE Comment_On_Message(
-  comment_id INTEGER PRIMARY KEY,
+  comment_id INTEGER GENERATED ALWAYS AS IDENTITY,
   comment_content VARCHAR2(255) NOT NULL,
   comment_is_read INTEGER NOT NULL,
   comment_sender_id INTEGER NOT NULL,
   comment_message_id INTEGER NOT NULL,
   comment_create_time VARCHAR2(30) NOT NULL,
+  CONSTRAINT pk_comment_on_message
+	PRIMARY KEY (comment_id),
   CONSTRAINT fk_comment_1 FOREIGN KEY
     (comment_sender_id) REFERENCES
       User_Public_Info(user_id)
@@ -294,18 +251,13 @@ CREATE TABLE Message_Collection(
 );
 ---------------------------------------------------
 --------------------Topic--------------------------
-CREATE SEQUENCE seq_topic
-MINVALUE 1
-NOMAXVALUE
-NOCYCLE
-START WITH 1
-INCREMENT BY 1
-CACHE 10;
 
 CREATE TABLE Topic(
-  topic_id INTEGER PRIMARY KEY,
+  topic_id INTEGER GENERATED ALWAYS AS IDENTITY,
   topic_heat INTEGER NOT NULL,
-  topic_content VARCHAR(50) NOT NULL
+  topic_content VARCHAR(50) NOT NULL,
+  CONSTRAINT pk_topic
+	PRIMARY KEY (topic_id)
 );
 
 CREATE INDEX t_heat_index ON TOPIC(topic_heat);
