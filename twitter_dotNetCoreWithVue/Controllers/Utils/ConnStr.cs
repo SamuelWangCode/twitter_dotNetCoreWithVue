21c51914ca0f469df8d9493eb110e7dc27a0f66c
Sky-1751862 <564318045@qq.com>
2019-07-07 16:21:03 +0800

    w

---
+++ twitter_dotNetCoreWithVue/Controllers/Utils/ConnStr.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace twitter_dotNetCoreWithVue.Controllers.Utils
+{
+    public class ConnStr
+    {
+        static public String getConnStr()
+        {
+
+            return "Data Source=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=localhost)(PORT=1521)))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=TwitterProject)));User Id=twitter;Password=19981229;"; // 使用自己的string
+
+        }
+    }
+}
