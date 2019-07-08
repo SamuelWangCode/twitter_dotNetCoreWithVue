6537cde5aa30e8e4ee318a0f0b62460bac3f7d16
Zhou <playerzhou@icloud.com>
2019-07-08 10:42:27 +0800

    update dbconf

--- twitter_dotNetCoreWithVue/Controllers/Utils/ConnStr.cs
+++ twitter_dotNetCoreWithVue/Controllers/Utils/ConnStr.cs
@@ -1,12 +1,15 @@
 ﻿using System;
+using System.IO;
 namespace twitter_dotNetCoreWithVue.Controllers.Utils
 {
+    
     public class ConnStr
     {
+        public static string line = new StreamReader("dbconf.cof").ReadLine();
         static public String getConnStr()
         {
-
-            return "Data Source=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=localhost)(PORT=1521)))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=TwitterProject)));User Id=twitter;Password=19981229;"; // 使用自己的string
+            return line;
+            return "Data Source=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=localhost)(PORT=1521)))(CONNECT_DATA=(SID=)));User Id=;Password=;"; // 使用自己的string
 
         }
     }
